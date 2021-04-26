2f1cae18f163b8bfd4b4b082cad3af5b57d41739
mohammads alehi <mamadreza8138@gmail.com>
2021-04-26 23:50:28 +0430

    'creat-model-task'

---
+++ tasks/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['complete']
